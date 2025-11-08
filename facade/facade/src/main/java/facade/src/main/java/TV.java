776a36778276f5e8d3bb03160dbfcd54985d491d
Erden20 <jomartereke@gmail.com>
2025-11-08 19:43:42 +0500

    Create TV.java

---
+++ facade/facade/src/main/java/facade/src/main/java/TV.java
@@ -0,0 +1,13 @@
+public class TV {
+    public void on() {
+        System.out.println("TV is ON");
+    }
+
+    public void off() {
+        System.out.println("TV is OFF");
+    }
+
+    public void setChannel(int channel) {
+        System.out.println("TV channel set to " + channel);
+    }
+}
