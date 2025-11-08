6fd22107e990c9dd053165d25888a34571f6ca85
Erden20 <jomartereke@gmail.com>
2025-11-08 19:44:13 +0500

    Create DVDPlayer.java

---
+++ facade/facade/src/main/java/facade/src/main/java/DVDPlayer.java
@@ -0,0 +1,21 @@
+public class DVDPlayer {
+    public void on() {
+        System.out.println("DVD Player is ON");
+    }
+
+    public void off() {
+        System.out.println("DVD Player is OFF");
+    }
+
+    public void play() {
+        System.out.println("DVD is playing");
+    }
+
+    public void pause() {
+        System.out.println("DVD paused");
+    }
+
+    public void stop() {
+        System.out.println("DVD stopped");
+    }
+}
