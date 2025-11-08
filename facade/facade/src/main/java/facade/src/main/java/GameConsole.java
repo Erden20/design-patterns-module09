756eddcd6c17ab4ec615c040d4394968405a7ceb
Erden20 <jomartereke@gmail.com>
2025-11-08 19:44:30 +0500

    Create GameConsole.java

---
+++ facade/facade/src/main/java/facade/src/main/java/GameConsole.java
@@ -0,0 +1,9 @@
+public class GameConsole {
+    public void on() {
+        System.out.println("Game Console is ON");
+    }
+
+    public void playGame() {
+        System.out.println("Game is starting");
+    }
+}
